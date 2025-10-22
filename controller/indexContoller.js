9e79a0b1af8660fdd25b23ba26fdb3b3d812832c
Triston Land <tristonland@gmail.com>
2025-10-22 14:21:05 -0600

    add crate game form

--- controller/indexContoller.js
+++ controller/indexContoller.js
@@ -1,5 +1,6 @@
 exports.sendIndex = (req, res, next) => {
   res.render("index", { stuff: "you can send stuff to ejs here" });
 };
-
-
+exports.createGame = (req, res, next) => {
+  res.render("createGame");
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,19 @@ const express = require("express");
 const indexContoller = require("../controller/indexContoller");
 const indexRouter = express.Router();
 
+indexRouter.get("/create/game", indexContoller.createGame);
 indexRouter.get("/", indexContoller.sendIndex);
 indexRouter.get("/{*error}", (req, res) => {
   res
     .status(404)
     .send("404 page not found. within index router (not controller)");
+});
+indexRouter.post("/{*error}", (req, res) => {
+  res
+    .status(404)
+    .send(
+      "could not post because route is not defined or is overwritten by index router"
+    );
 });
 
 module.exports = { indexRouter };
